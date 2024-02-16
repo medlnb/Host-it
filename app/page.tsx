26ade2fb853662c15c94de0a17c55b1083639ac0
TWSU <mohamedlanabi0@gmail.com>
2024-02-16 18:37:46 +0100

    Fix: again vercel stupid error

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-"use client";
+// "use client";
 import Posts from "@components/Posts";
 import "@styles/App.css";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 
 function App() {
-  const searchParams = useSearchParams();
-  const params = new URLSearchParams(searchParams);
-
-  return (
-    <div className="homepage--container">
-      {<Posts searchParams={{ query: params.get("query") ?? undefined }} />}
-    </div>
-  );
+  // const searchParams = useSearchParams();
+  // const params = new URLSearchParams(searchParams);
+  // searchParams={{ query: params.get("query") ?? undefined }}
+  return <div className="homepage--container">{<Posts />}</div>;
 }
 
 export default App;

e3af8d7063ea7d1917eb2e05657489fc835cbd58
TWSU <mohamedlanabi0@gmail.com>
2024-03-11 20:49:43 +0100

    test 100000

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -4,34 +4,27 @@ import { getServerSession } from "next-auth/next";
 
 async function Layout({ children }: { children: ReactNode }) {
   const session = await getServerSession(options);
-
+  if (!session) console.log("helllo");
   return (
-    <>
-      <p>hello</p>
-      {session ? (
-        <div className="max-w-full my-0 mx-auto mt-2 mr-4 mb-16 ml-2">
-          {children}
-          <div className="fixed flex bottom-0 left-1/2 transform -translate-x-1/2 justify-center items-center gap-4 p-4 border-t border-gray-400 bg-white">
-            <a href="mailto:  mohamedlanabi0@gmail.com">Email</a>
-            <a href="tel:  +212 6 99 99 99 99">Phone</a>
-            <a href="https://www.linkedin.com/in/mohamed-lanabi-3b7b8a1b9/">
-              LinkedIn
-            </a>
-            <a
-              href="
+    <div className="max-w-full my-0 mx-auto mt-2 mr-4 mb-16 ml-2">
+      {children}
+      <div className="fixed flex bottom-0 left-1/2 transform -translate-x-1/2 justify-center items-center gap-4 p-4 border-t border-gray-400 bg-white">
+        <a href="mailto:  mohamedlanabi0@gmail.com">Email</a>
+        <a href="tel:  +212 6 99 99 99 99">Phone</a>
+        <a href="https://www.linkedin.com/in/mohamed-lanabi-3b7b8a1b9/">
+          LinkedIn
+        </a>
+        <a
+          href="
           https://www.linkedin.com/in/mohamed-lanabi-3b7b8a1b9/"
-            >
-              Github
-            </a>
-            <a href="https://www.linkedin.com/in/mohamed-lanabi-3b7b8a1b9/">
-              Instagram
-            </a>
-          </div>
-        </div>
-      ) : (
-        "leave bro"
-      )}
-    </>
+        >
+          Github
+        </a>
+        <a href="https://www.linkedin.com/in/mohamed-lanabi-3b7b8a1b9/">
+          Instagram
+        </a>
+      </div>
+    </div>
   );
 }
 

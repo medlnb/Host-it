887163deb0d7ef4ea3a823e010361bd5fbcf6371
medlnb <mohamedlanabi0@gmail.com>
2024-04-16 13:04:04 +0100

    test:

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,9 +1,9 @@
-import { options } from "@app/api/auth/[...nextauth]/options";
-import { getServerSession } from "next-auth";
+// import { options } from "@app/api/auth/[...nextauth]/options";
+// import { getServerSession } from "next-auth";
 
 async function Layout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(options); // database connection is to slow, so it takes time to get the user info
-  console.log("session", session);
+  // const session = await getServerSession(options); // database connection is to slow, so it takes time to get the user info
+  // console.log("session", session);
   return (
     <div className="max-w-full my-0 mx-auto mt-2 mr-4 mb-16 ml-2">
       {/* {session === null ? "not allowed" : children} */}

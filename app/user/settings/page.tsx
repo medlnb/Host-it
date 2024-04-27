1bbdc2a54987f803efdff268995423b72ed14f6d
medlnb <mohamedlanabi0@gmail.com>
2024-04-27 11:05:24 +0100

    test: server session

---
+++ app/user/settings/page.tsx
@@ -0,0 +1,10 @@
+import { options } from "@app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+
+async function Page() {
+  const session = await getServerSession(options); // database connection is too slow, so it takes time to get the user info
+  console.log("session", session);
+  return <div>asd</div>;
+}
+
+export default Page;

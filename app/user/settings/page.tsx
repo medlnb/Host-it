337a3995b4cba0e1796896c0e334f0c62a94798d
medlnb <mohamedlanabi0@gmail.com>
2024-04-27 12:28:48 +0100

    test

--- app/user/settings/page.tsx
+++ app/user/settings/page.tsx
@@ -1,14 +1,11 @@
-"use client";
 import { options } from "@app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
 
-function Page() {
-  const getsession = async () => {
-    const session = await getServerSession(options); // database connection is too slow, so it takes time to get the user info
-    console.log("session", session);
-  };
-  getsession();
-  return <div>asd</div>;
+async function Page() {
+  const session = await getServerSession(options); // database connection is too slow, so it takes time to get the user info
+  console.log("session", session);
+
+  return <div>settings</div>;
 }
 
 export default Page;
